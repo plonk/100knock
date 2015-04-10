be9e3ad57d4f3443d454707693c3339ae20b27e9
Yoteichi <plonk@piano.email.ne.jp>
2015-04-11 01:10:29 +0900

    q47.rbのリファクタリング

--- q47.rb
+++ q47.rb
@@ -32,11 +32,13 @@
 
 require_relative 'q41'
 
-def print_row(triple)
-  verbal_phrase, particles, chunks = triple
-  pstr = particles.join(' ')
-  cstr = chunks.join(' ')
-  puts [verbal_phrase, pstr, cstr].join("\t")
+
+require 'binding_of_caller'
+
+class Symbol
+  def as_method(receiver = binding.of_caller(1).eval('self'))
+    receiver.method(self)
+  end
 end
 
 class Hash
@@ -46,28 +48,75 @@ class Hash
     end
   end
 
+  def to_pred
+    -> obj { all? { |k,v| v === obj.send(k) } }
+  end
+end
+
+alias edges pair_up
+
+def time(label)
+  b = Time.now
+  yield.tap do
+    STDERR.puts "#{label}: #{((Time.now - b)*1000).round} milliseconds"
+  end
+end
+
+# --------------------------------------------------------------------
+
+def render_row(verbal_phrase, particles, chunks)
+  [verbal_phrase, particles.join(' '), chunks.join(' ')].join("\t")
+end
+
+Verbal_noun_p = { pos: '名詞', pos1: 'サ変接続' }.to_pred
+Particle_wo_p = { pos: '助詞', base: 'を' }.to_pred
+Particle_p = proc { |m| m.pos == '助詞' }
+Verb_p = proc { |m| m.pos == '動詞' }
+
+# [Chunk, Chunk] -> Bool
+def light_verb_construction?(c1, c2)
+  return false unless c1.morphs.size==2
+
+  fst, snd = c1.morphs
+  return Verbal_noun_p.(fst) && Particle_wo_p.(snd) && c2.morphs.any?(&Verb_p)
+end
+
+# [Morph*] → Morph or nil
+def last_particle(morphs)
+  morphs.reverse_each.find(&Particle_p)
+end
+
+# [Chunk,Chunk] → [[String, [String*], [String*]]*]
+def to_row((c1, c2), environ)
+  deps = environ.values_at(*c2.srcs).reject { |c| c==c1 }
+    .select { |c| c.morphs.any?(&Particle_p) }
+
+  dep_particles, dep_phrases = deps.map { |c| [last_particle(c.morphs).base, c.unpunctuated_text] }
+    .sort_by(&:first).transpose
+
+  lv_construction = "#{c1.unpunctuated_text}#{c2.morphs.find(&Verb_p).base}"
+  dep_particles ||= []
+  dep_phrases ||= []
+
+  [lv_construction, dep_particles, dep_phrases]
+end
+
+#             動詞構文, 係る助詞,  係る文節
+# [Chunk*] → [[String, [String*], [String*]]*]
+def light_verb_constructions(sentence)
+  edges(sentence)
+    .select { |c1,c2| light_verb_construction?(c1, c2) }
+    .map { |chunks| to_row(chunks, sentence) }
 end
 
 def main
-  doc = load_document
+  doc = time("load document") { load_document }
 
-  triples = doc.flat_map { |cs|
-    pair_up(cs).select { |c1, c2|
-      left = c1.morphs
-      right = c2.morphs
-
-      (left.size==2 &&
-       { pos: '名詞', pos1: 'サ変接続' } === left[0] &&
-       { pos: '助詞', base: 'を' } === left[1]) &&
-      (right.any? { |m| m.pos == '動詞' })
-    }.map { |c1, c2|
-      deps = cs.values_at(*c2.srcs).select { |c| c.morphs.any? { |m| m.pos=='助詞' } && c!=c1}
-        .sort_by { |c| c.morphs.reverse.find { |m| m.pos=='助詞' }.base }
-      particles = deps.map { |c| c.morphs.reverse.find { |m| m.pos=='助詞' } }.map(&:base)
-      phrases = deps.map { |c| c.unpunctuated_text }
-      ["#{c1.unpunctuated_text}#{c2.morphs.find { |m| m.pos=='動詞' }.base}", particles, phrases]
-    }
-  }.each(&method(:print_row))
+  doc.flat_map { |sent|
+    light_verb_constructions(sent)
+  }.each do |row|
+    puts render_row(*row)
+  end
 end
 
 main
