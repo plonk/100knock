8e2723c441d0a471c31a2eba8cd2b064fb11392e
Yoteichi <plonk@piano.email.ne.jp>
2015-04-11 09:39:56 +0900

    抽出じゃなくて置換だった

--- q56.rb
+++ q56.rb
@@ -11,16 +11,66 @@ Stanford Core NLPの共参照解析の結果に基づき，文中の参照表現
 
 require 'nokogiri'
 
-def main
-  doc = File.open('nlp.txt.xml', &Nokogiri.method(:XML))
-  corefs = doc.css('document > coreference').first
-  corefs.css('coreference').each do |coref|
-    mr = coref.css('mention[representative=true]').first
-    others = coref.css('mention:not([representative=true])').map { |m| m.css('text').first.text }
-    
-    puts "#{mr.css('text').text} (#{others.join('|')})"
+class Mention
+  def self.from_elt(elt)
+    Mention.new.tap do |m|
+      m.representative = elt['representative']=='true'
+      m.sentence = elt.css('sentence').first.text.to_i
+      m.start    = elt.css('start').first.text.to_i
+      m.end      = elt.css('end').first.text.to_i
+      m.head     = elt.css('head').first.text.to_i
+      m.text     = elt.css('text').first.text
+    end
+  end
+
+  # 文字位置の半開区間に変換する。
+  def to_offsets(doc)
+    offset_begin = doc.css("sentence##{sentence} > tokens > token##{start}").css('CharacterOffsetBegin').first.text.to_i
+    offset_end   = doc.css("sentence##{sentence} > tokens > token##{self.end-1}").css('CharacterOffsetEnd').first.text.to_i
+    [offset_begin, offset_end]
+  end
+
+  attr_accessor :representative, :sentence, :start, :end, :head, :text
+end
+
+class Coreference
+  def self.from_elt(elt)
+    Coreference.new.tap do |c|
+      c.mentions = elt.css('mention').map { |mention| Mention.from_elt(mention) }
+    end
+  end
+
+  attr_accessor :mentions
+end
+
+class Program
+  attr_accessor :doc
+
+  def replace(text, corefs)
+    plan = corefs.flat_map do |coref|
+      rep = coref.mentions.find(&:representative)
+      plan = coref.mentions.reject(&:representative).map do |mention|
+        a, b = mention.to_offsets(doc)
+        [a, b, "【#{rep.text} 《#{mention.text}》】"]
+      end
+    end.sort_by(&:first).reverse
+ 
+    plan.each do |a, b, alt_text|
+      text[a ... b] = alt_text
+    end
+    text
+  end
+
+  def main
+    self.doc = File.open('nlp.txt.xml', &Nokogiri.method(:XML))
+    corefs = doc.css('document > coreference').first
+      .css('coreference').map { |coref| Coreference.from_elt(coref) }
+    text = File.open('nlp.txt') do |f|
+      f.read
+    end
+    print replace(text, corefs)
   end
 end
 
-main if __FILE__ == $0
+Program.new.main if __FILE__ == $0
 
