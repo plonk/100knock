833cc0e8194f83b85efab9265b05fdced21f2109
Yoteichi <plonk@piano.email.ne.jp>
2015-04-12 03:25:45 +0900

    エッジに関係のタイプを表わすラベルを付けるようにした

--- q57.rb
+++ q57.rb
@@ -12,38 +12,52 @@ Graphvizを用いるとよい．また，Pythonから有向グラフを直接的
 require 'nokogiri'
 require 'graphviz'
 
+class Edge < Struct.new(:from, :to, :type)
+  def self.[](*args)
+    Edge.new(*args)
+  end
+end
+
 # deps: [[[String,String], [String,String]]*]
-def output(deps, filename)
+def output(edges, filename)
   g = GraphViz.new(:G, type: :digraph)
 
   nodes = {}
-  deps.flatten(1).uniq.each do |idx, label|
+  edges.flat_map { |e| [e.from, e.to] }.uniq.each do |idx, label|
     nodes[idx] = g.add_nodes(idx, label: label)
   end
 
-  deps.each do |(gov_idx, gov_label), (dep_idx, dep_label)|
-    g.add_edges(nodes[gov_idx], nodes[dep_idx])
+  edges.each do |e|
+    gov_idx, = e.from
+    dep_idx, = e.to
+    g.add_edges(nodes[gov_idx], nodes[dep_idx], label: e.type)
   end
 
   g.output(png: filename)
 end
 
+def to_graphs(doc)
+  doc.css('document > sentences > sentence').map do |sentence|
+    sentence.css('dependencies[type=collapsed-dependencies] > dep').map do |dep|
+      gov = dep.css('governor').first
+      dependent = dep.css('dependent').first
+      type = dep['type']
+
+      Edge[[gov['idx'], gov.text], [dependent['idx'], dependent.text], ' '+type]
+    end
+  end
+end
+
 def main
   doc = File.open('nlp.txt.xml', &Nokogiri.method(:XML))
 
+  graphs = to_graphs(doc)
+
   Dir.mkdir 'q57' unless File.directory? 'q57'
-  deps = doc.css('document > sentences > sentence').each do |sentence|
-    deps = sentence.css('dependencies[type=collapsed-dependencies] > dep').map do |dep|
-      gov = dep.css('governor').first
-      dependent = dep.css('dependent').first
-
-      [[gov['idx'], gov.text], [dependent['idx'], dependent.text]]
-    end
-
-    puts "q57/sentence#{sentence['id']}.png"
-    output(deps, "q57/sentence#{sentence['id']}.png")
+  graphs.each.with_index(1) do |edges, i|
+    puts "q57/sentence#{i}.png"
+    output(edges, "q57/sentence#{i}.png")
   end
-
 end
 
 main if __FILE__ == $0
